5de47997f5ebebad0ef6d1a979286fff94569511
malo-dev <leadermushio377@gmail.com>
2022-10-03 13:24:34 +0200

    Add regex

--- css-course/learnJs/script.js
+++ css-course/learnJs/script.js
@@ -15,3 +15,7 @@ makeServerRequest.then(result => {
 makeServerRequest.catch(error => {
   console.log(error)
 })
+// regew freecode camp
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
