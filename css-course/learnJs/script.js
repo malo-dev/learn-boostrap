da38181014e698392cc3570e44fc28d45fd68149
malo-dev <leadermushio377@gmail.com>
2022-10-03 11:55:11 +0200

    adding an other message

--- css-course/learnJs/script.js
+++ css-course/learnJs/script.js
@@ -1,5 +1,17 @@
-let prims = 1;
- prims = 2;
-prims = 3;
-prims = prims + prims
- console.log(prims)
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+makeServerRequest.catch(error => {
+  console.log(error)
+})
